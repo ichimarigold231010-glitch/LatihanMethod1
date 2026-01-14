76e9d99533255b2b8a278aa93435d244890424bc
ichimarigold231010-glitch <ichimarigold231010@gmail.com>
2026-01-14 14:18:01 +0700

    menambahkan tambah dan kurang

--- LatihanMethod1/Program.cs
+++ LatihanMethod1/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿int a = 10, b= 2, c = 0;
+
+void tambah ()
+{
+    c = a + b;
+    Console.WriteLine ($"{a} + {b} = {c}");
+}
+
+void kurang()
+{
+    c = a - b;
+    Console.WriteLine($"{a} - {b} = {c}");
+}
